be7343fc3863f3b80f0b72dbbc72c895a7f413bd
rsalbergaria <116372909+rsalbergaria@users.noreply.github.com>
2024-06-09 18:17:11 -0300

    Update validar_recursos_zona_a.sh

--- scripts/validar_recursos_zona_a.sh
+++ scripts/validar_recursos_zona_a.sh
@@ -12,30 +12,33 @@ else
   echo ">[ERRO] Tenho um problema ao retornar a subnet da zona a. Será se existe uma subnet na zona A?"
 fi
 
-security_group_id=$(aws ec2 describe-security-groups --group-names "bia-dev" --query "SecurityGroups[0].GroupId" --output text 2>/dev/null)
-if [ $? -eq 0 ]; then
-  echo "[OK] Security Group bia-dev foi criado"
+# Buscar o ID do security group 'bia-dev'
+security_group_id=$(aws ec2 describe-security-groups --filters "Name=group-name,Values=bia-dev" --query "SecurityGroups[0].GroupId" --output text 2>/dev/null)
+
+if [ $? -eq 0 ] && [ -n "$security_group_id" ]; then
+  echo "[OK] Security Group 'bia-dev' foi encontrado com ID $security_group_id"
   
   # Validar inbound rule para o security group 'bia-dev'
-  inbound_rule=$(aws ec2 describe-security-groups --group-ids $security_group_id --filters "Name=ip-permission.from-port,Values=3001" --filters "Name=ip-permission.cidr,Values=0.0.0.0/0" --output text)
+  inbound_rule=$(aws ec2 describe-security-groups --group-ids $security_group_id --filters "Name=ip-permission.from-port,Values=3001" "Name=ip-permission.cidr,Values=0.0.0.0/0" --query "SecurityGroups[0].IpPermissions" --output text)
 
   if [ -n "$inbound_rule" ]; then
-    echo " [OK] Regra de entrada está ok"
+    echo "[OK] Regra de entrada para a porta 3001 está correta"
   else
-    echo " >[ERRO] Regra de entrada para a porta 3001 não encontrada ou não está aberta para o mundo todo. Reveja a aula do Henrylle"
+    echo "[ERRO] Regra de entrada para a porta 3001 não encontrada ou não está aberta para o mundo todo. Reveja a aula do Henrylle"
   fi
 
   # Validar outbound rule para o security group 'bia-dev'
-  outobund_rule=$(aws ec2 describe-security-groups --group-ids $security_group_id --query "SecurityGroups[0].IpPermissionsEgress[?IpProtocol=='-1' && IpRanges[0].CidrIp=='0.0.0.0/0']" --output text)
+  outbound_rule=$(aws ec2 describe-security-groups --group-ids $security_group_id --query "SecurityGroups[0].IpPermissionsEgress[?IpProtocol=='-1' && IpRanges[0].CidrIp=='0.0.0.0/0']" --output text)
   
-  if [ -n "$outobund_rule" ]; then
-    echo " [OK] Regra de saída está correta"
+  if [ -n "$outbound_rule" ]; then
+    echo "[OK] Regra de saída está correta"
   else
-    echo " >[ERRO] Regra de saída para o mundo não encontrada. Reveja a aula do Henrylle"
+    echo "[ERRO] Regra de saída para o mundo não encontrada. Reveja a aula do Henrylle"
   fi
 else
-  echo ">[ERRO] Não achei o security group bia-dev. Ele foi criado?"
+  echo "[ERRO] Não achei o security group 'bia-dev'. Ele foi criado?"
 fi
+
 
 if aws iam get-role --role-name role-acesso-ssm &>/dev/null; then
     echo "[OK] Tudo certo com a role 'role-acesso-ssm'"

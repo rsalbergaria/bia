57e2655f11ad0c0173d0676395a05eb845ed465c
rsalbergaria <116372909+rsalbergaria@users.noreply.github.com>
2024-06-23 22:16:08 -0300

    Update user_data_ec2_zona_a.sh to install plsql

--- scripts/user_data_ec2_zona_a.sh
+++ scripts/user_data_ec2_zona_a.sh
@@ -1,32 +1,42 @@
 #!/bin/bash
 
-#Instalar Docker e Git
+# Atualizar pacotes e instalar Docker e Git
 sudo yum update -y
 sudo yum install git -y
 sudo yum install docker -y
+
+# Adicionar usuários ao grupo Docker
 sudo usermod -a -G docker ec2-user
 sudo usermod -a -G docker ssm-user
 id ec2-user ssm-user
 sudo newgrp docker
 
-#Ativar docker
+# Ativar e iniciar Docker
 sudo systemctl enable docker.service
 sudo systemctl start docker.service
 
-#Instalar docker compose 2
+# Instalar Docker Compose 2
 sudo mkdir -p /usr/local/lib/docker/cli-plugins
 sudo curl -SL https://github.com/docker/compose/releases/download/v2.23.3/docker-compose-linux-x86_64 -o /usr/local/lib/docker/cli-plugins/docker-compose
 sudo chmod +x /usr/local/lib/docker/cli-plugins/docker-compose
 
-
-#Adicionar swap
+# Adicionar swap
 sudo dd if=/dev/zero of=/swapfile bs=128M count=32
 sudo chmod 600 /swapfile
 sudo mkswap /swapfile
 sudo swapon /swapfile
-sudo echo "/swapfile swap swap defaults 0 0" >> /etc/fstab
+echo "/swapfile swap swap defaults 0 0" | sudo tee -a /etc/fstab
 
-
-#Instalar node e npm
+# Instalar Node.js e npm
 curl -fsSL https://rpm.nodesource.com/setup_21.x | sudo bash -
 sudo yum install -y nodejs
+
+# Instalar PostgreSQL Client (psql)
+sudo yum install -y postgresql
+
+# Verificar versões instaladas
+docker --version
+docker-compose --version
+node --version
+npm --version
+psql --version
